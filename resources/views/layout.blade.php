1ed05f71e549f67295f7cc7ca463c87556c6598b
Juan Pablo Villaplana <jpvillaplana@gmail.com>
2019-06-15 18:06:53 -0600

    Quitar numeros del footer

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -87,5 +87,3 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
-
-T3m8au43
